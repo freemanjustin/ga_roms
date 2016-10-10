e728229340c595da8d6678546363eeeb4e5bc775
@_freej_ <freeman.justin@gmail.com>
2016-10-10 14:52:37 +1100

    updated to read rmse

--- run_it/ga_lib/rank_population.py
+++ run_it/ga_lib/rank_population.py
@@ -16,6 +16,10 @@ def rank_population(exp_name, generation, population):
         run_cmd(rmse_cmd)
 
     # and rank them
-    #rank_pop_cmd = "./rank_pop %s %d %d" % (exp_name, generation, population)
-    #run_cmd(gen_drag_cmd)
+    # read in each rmse file
+    for i in range(0,population):
+        rmse_filename = "%s/%04d/%04d/rmse.txt" % (exp_name, generation, i)
+        rmse_file = open(rmse_filename, 'r')
+        rmse = rmse_file.readlines()
+        print rmse
     return
