f6dfde45fb76c444d0ab65254feb953e0d490a84
@_freej_ <freeman.justin@gmail.com>
2016-10-10 14:01:05 +1100

    adding cost function to ga suite

--- run_it/ga_lib/config.py
+++ run_it/ga_lib/config.py
@@ -1,6 +1,8 @@
 # config variables for the ROMS runs
 # roms bin data is where the input text files live
-roms_bin_data = "roms_bin_data/"
+# need full path here
+roms_bin_data = "./roms_bin_data/"
 
 # roms static data is where the forcing and bc data is
-roms_static_data = "roms_static_data/"
+# need full path here
+roms_static_data = "./roms_static_data/"
